7fe4d32f145feaa51d34b771f445f0750a17de0f
ScaryOrange <UnKindledastor@yandex.ru>
2023-04-21 17:30:37 +0300

    upd web form

--- flaskTG_edit_db_request.py
+++ flaskTG_edit_db_request.py
@@ -2,7 +2,8 @@ import flask
 from flask import url_for
 from flask import request
 from flask import Flask
-
+from data import db_session
+from data.form import Form
 
 app = Flask(__name__)
 
@@ -12,14 +13,21 @@ def db_mod():
     if request.method == 'GET':
         return flask.render_template('db_edition_from.html', title='Ваши вопросы')
     elif request.method == 'POST':
-        print(request.form['username'])
-        print(request.form['question'])
-        print(request.form['correct_answer'])
-        print(request.form['incorrect_answer1'])
-        print(request.form['incorrect_answer2'])
-        print(request.form['incorrect_answer3'])
-        return 'Спс'
+        db_sess = db_session.create_session()
+        new_quest = Form()
+        if not request.form['question'] or not request.form['username'] or not request.form['correct_answer']:
+            return flask.render_template('db_edition_from.html', title='заполните все джанные')
+        new_quest.question = request.form['question']
+        new_quest.username = request.form['username']
+        new_quest.correct_answer = request.form['correct_answer']
+        new_quest.other_answer1 = request.form['incorrect_answer1']
+        new_quest.other_answer2 = request.form['incorrect_answer2']
+        new_quest.other_answer3 = request.form['incorrect_answer3']
+        db_sess.add(new_quest)
+        db_sess.commit()
+        return 'Спасибо'
 
 
 if __name__ == '__main__':
+    db_session.global_init("rating/rating.db")
     app.run(port=8080, host='127.0.0.1')
